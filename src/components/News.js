49fa065f8739153e9e219e25823d118f7cfcd20b
kinshukBiswas <kinshukBiswas1611@gmail.com>
2026-01-05 14:19:02 +0530

    setting component structure of NewsMonkey app.

---
+++ src/components/News.js
@@ -0,0 +1,23 @@
+// eslint-disable-next-line
+import PropTypes from "prop-types";
+import React, { Component } from "react";
+import NewsComp1 from "./NewsComp1";
+
+export default class News extends Component {
+  render() {
+    return (
+      <div>
+        <NewsComp1 />
+        <NewsComp1 />
+        <NewsComp1 />
+        <NewsComp1 />
+        <NewsComp1 />
+        <NewsComp1 />
+        <NewsComp1 />
+        <NewsComp1 />
+
+        <NewsComp1 />
+      </div>
+    );
+  }
+}
---
+++ src/components/NewsComp1.js
@@ -0,0 +1,9 @@
+// eslint-disable-next-line
+import PropTypes from "prop-types";
+import React, { Component } from "react";
+
+export default class NewsComp1 extends Component {
+  render() {
+    return <div>NewsComp1</div>;
+  }
+}
