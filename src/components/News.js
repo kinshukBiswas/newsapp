66cf897061a2c8dc5d7a89fca088a1dd76e1742c
kinshukBiswas <kinshukBiswas1611@gmail.com>
2026-01-06 16:16:43 +0530

    Add imageUrl prop to NewsItem component for dynamic image rendering

--- src/components/News.js
+++ src/components/News.js
@@ -13,6 +13,7 @@ export default class News extends Component {
             <NewsItem
               title="News Title"
               description="my Description for the news. this will be changed later as per news."
+              imageUrl="https://platform.theverge.com/wp-content/uploads/sites/2/chorus/uploads/chorus_asset/file/23318435/akrales_220309_4977_0232.jpg?quality=90&strip=all&crop=0%2C10.732984293194%2C100%2C78.534031413613&w=1200"
             />
           </div>
           <div className="col-md-4 my-2">
