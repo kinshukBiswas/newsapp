cfb263770a7c32451b4d4f407b8fe8c2ad03a933
kinshukBiswas <kinshukBiswas1611@gmail.com>
2026-01-10 17:11:24 +0530

    Update News component to fetch articles about India (as an example) and include total results in state

--- src/components/News.js
+++ src/components/News.js
@@ -10,18 +10,33 @@ export default class News extends Component {
       articles: [],
       loading: true,
       page: 1,
+      totalResults: 0,
     };
   }
 
   async componentDidMount() {
-    let url = `https://newsapi.org/v2/everything?q=cricket&apiKey=cf8c035a536a432597a385b4e0eb7d28&page=1&pageSize=20`;
+    let url = `https://newsapi.org/v2/everything?q=india&from=${new Date(
+      Date.now() - 86400000
+    ).toLocaleDateString(
+      "en-CA"
+    )}&sortBy=publishedAt&apiKey=cf8c035a536a432597a385b4e0eb7d28&page=${
+      this.state.page
+    }&pageSize=20`;
     let data = await fetch(url);
     let parsedData = await data.json();
-    this.setState({ articles: parsedData.articles, loading: false });
+    this.setState({
+      articles: parsedData.articles,
+      totalResults: parsedData.totalResults,
+      loading: false,
+    });
   }
 
   handlePrev = async () => {
-    let url = `https://newsapi.org/v2/everything?q=cricket&apiKey=cf8c035a536a432597a385b4e0eb7d28&page=${
+    let url = `https://newsapi.org/v2/everything?q=india&from=${new Date(
+      Date.now() - 86400000
+    ).toLocaleDateString(
+      "en-CA"
+    )}&sortBy=publishedAt&apiKey=cf8c035a536a432597a385b4e0eb7d28&page=${
       this.state.page - 1
     }&pageSize=20`;
     let data = await fetch(url);
@@ -29,12 +44,17 @@ export default class News extends Component {
     this.setState({
       articles: parsedData.articles,
       page: this.state.page - 1,
+      totalResults: parsedData.totalResults,
       loading: false,
     });
   };
 
   handleNext = async () => {
-    let url = `https://newsapi.org/v2/everything?q=cricket&apiKey=cf8c035a536a432597a385b4e0eb7d28&page=${
+    let url = `https://newsapi.org/v2/everything?q=india&from=${new Date(
+      Date.now() - 86400000
+    ).toLocaleDateString(
+      "en-CA"
+    )}&sortBy=publishedAt&apiKey=cf8c035a536a432597a385b4e0eb7d28&page=${
       this.state.page + 1
     }&pageSize=20`;
     let data = await fetch(url);
@@ -42,6 +62,7 @@ export default class News extends Component {
     this.setState({
       articles: parsedData.articles,
       page: this.state.page + 1,
+      totalResults: parsedData.totalResults,
       loading: false,
     });
   };
@@ -72,14 +93,20 @@ export default class News extends Component {
         </div>
         <div className="container d-flex justify-content-between">
           <button
-            disabled={this.state.page == 1}
+            disabled={this.state.page === 1}
             className="btn btn-dark justify-content-between"
             onClick={this.handlePrev}
           >
             &larr; Previous
           </button>
           <button
-            disabled={this.state.page == 5}
+            disabled={
+              (this.state.totalResults &&
+                this.state.page >= Math.ceil(this.state.totalResults / 20)) ||
+              this.state.totalResults === 0 ||
+              this.state.articles.length === 0 ||
+              this.state.page >= 5
+            }
             className="btn btn-dark d-flex justify-content-between"
             onClick={this.handleNext}
           >
