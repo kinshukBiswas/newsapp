f817db6f97586c8a564d80568adfd6798819a12e
kinshukBiswas <kinshukBiswas1611@gmail.com>
2026-02-03 14:21:24 +0530

    adding category to News in App.js; removing width from news items; installing react-router-dom

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ export default class App extends Component {
     return (
       <div>
         <Navbar />
-        <News category="sports" />
+        <News category="business" />
       </div>
     );
   }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -7,7 +7,7 @@ export default class NewsComp1 extends Component {
     let { title, description, imageUrl, newsUrl } = this.props;
     return (
       <div>
-        <div className="card" style={{ width: "18rem" }}>
+        <div className="card">
           <img src={imageUrl} className="card-img-top" alt="" />
           <div className="card-body">
             <h5 className="card-title" title={title}>
