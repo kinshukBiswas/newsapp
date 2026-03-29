dfcf5f129bf23a5340c4deab137a6db5eca94f54
kinshukBiswas <kinshukBiswas1611@gmail.com>
2026-03-29 15:50:01 +0530

    Update README and configuration files; add deployment scripts and devDependencies for gh-pages.

--- README.md
+++ README.md
@@ -15,7 +15,7 @@ You can clone this github project using (ensure that git is installed in your co
 git clone https://github.com/kinshukBiswas/newsapp.git
 ```
 
-After cloning (only if _node and npm_ is installed, else download it), write the following in your terminal of that folder where it is cloned:
+After cloning (only if _node and npm_ is install, else download it), write the following in your terminal of that folder where it is cloned:
 
 ```bash
 npm install .
@@ -33,4 +33,4 @@ to install all the `node_modules` folders.
 ## Conversion to Function-based component
 
 The project is changed to a function-based component
-Click [here](https://github.com/kinshukBiswas/newsapp/commit/6c2cc50a59e85c58b4f39239fd9ae143b76d3bd0) to see the final class based component version of this project. (Note that some changes will be made in the function-based component project in the next few commits, so, those commits will remain a little bit distinct from the class-based component.)
+Click [here](https://github.com/kinshukBiswas/newsapp/commit/6c2cc50a59e85c58b4f39239fd9ae143b76d3bd0) to see the final class based component version of this project. (Note that some changes will be made in the function-based component project in the next and final commit, so, that commit will remain a little bit distinct from the class-based component. This commit is containing equivalent code to the class-based component or the previous commit.)
--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-// eslint-disable-next-line
+
 import React, { Component, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
@@ -20,100 +20,86 @@ export default function App() {
   };
 
   return (
-    <Router>
-      <div>
-        <Navbar />
-        <LoadingBar height={3} color="#f11946" progress={state.progress} />
-        <Routes>
-          <Route
-            exact
-            path="/"
-            element={
-              <News
-                setProgress={setProgress}
-                key="general"
-                category="general"
-              />
-            }
-          />
-          <Route
-            exact
-            path="/business"
-            element={
-              <News
-                setProgress={setProgress}
-                key="business"
-                category="business"
-              />
-            }
-          />
-          <Route
-            exact
-            path="/entertainment"
-            element={
-              <News
-                setProgress={setProgress}
-                key="entertainment"
-                category="entertainment"
-              />
-            }
-          />
-          <Route
-            exact
-            path="/health"
-            element={
-              <News setProgress={setProgress} key="health" category="health" />
-            }
-          />
-          <Route
-            exact
-            path="/science"
-            element={
-              <News
-                setProgress={setProgress}
-                key="science"
-                category="science"
-              />
-            }
-          />
-          <Route
-            exact
-            path="/sports"
-            element={
-              <News setProgress={setProgress} key="sports" category="sports" />
-            }
-          />
-          <Route
-            exact
-            path="/cricket"
-            element={
-              <News
-                setProgress={setProgress}
-                key="cricket"
-                category="cricket"
-              />
-            }
-          />
-          <Route
-            exact
-            path="/technology"
-            element={
-              <News
-                setProgress={setProgress}
-                key="technology"
-                category="technology"
-              />
-            }
-          />
-          <Route
-            exact
-            path="/anime"
-            element={
-              <News setProgress={setProgress} key="anime" category="anime" />
-            }
-          />
-        </Routes>
-      </div>
-    </Router>
+    <div>
+      <Navbar />
+      <LoadingBar height={3} color="#f11946" progress={state.progress} />
+      <Routes>
+        <Route
+          exact
+          path="/"
+          element={
+            <News setProgress={setProgress} key="general" category="general" />
+          }
+        />
+        <Route
+          exact
+          path="/business"
+          element={
+            <News
+              setProgress={setProgress}
+              key="business"
+              category="business"
+            />
+          }
+        />
+        <Route
+          exact
+          path="/entertainment"
+          element={
+            <News
+              setProgress={setProgress}
+              key="entertainment"
+              category="entertainment"
+            />
+          }
+        />
+        <Route
+          exact
+          path="/health"
+          element={
+            <News setProgress={setProgress} key="health" category="health" />
+          }
+        />
+        <Route
+          exact
+          path="/science"
+          element={
+            <News setProgress={setProgress} key="science" category="science" />
+          }
+        />
+        <Route
+          exact
+          path="/sports"
+          element={
+            <News setProgress={setProgress} key="sports" category="sports" />
+          }
+        />
+        <Route
+          exact
+          path="/cricket"
+          element={
+            <News setProgress={setProgress} key="cricket" category="cricket" />
+          }
+        />
+        <Route
+          exact
+          path="/technology"
+          element={
+            <News
+              setProgress={setProgress}
+              key="technology"
+              category="technology"
+            />
+          }
+        />
+        <Route
+          exact
+          path="/anime"
+          element={
+            <News setProgress={setProgress} key="anime" category="anime" />
+          }
+        />
+      </Routes>
+    </div>
   );
 }
